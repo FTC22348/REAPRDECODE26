a495fe4e087cb666318f24f5d7aa9231ca255883
Nich Sembley <114117177+NicholasIGuess@users.noreply.github.com>
2025-12-04 18:51:04 -0500

    thank you davis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Flicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Flicker.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Flicker {
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
+
+public class Flicker implements Subsystem {
+    public static final Flicker INSTANCE = new Flicker();
+    private Flicker() { }
+    private ServoEx akon = new ServoEx("akon");
+    public Command smackThat = new SetPosition(akon, 1);
+    public Command allOnTheFloor = new SetPosition(akon, 0);
 }
